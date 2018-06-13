d38303d0175d1a830ffd4f4d07a06a5d3b6635ad
Mukhammadali <toshboev7@gmail.com>
2018-06-13 23:54:21 +0900

    Abstract Class is done

--- Abstract_class.cpp
+++ Abstract_class.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
-class Person {
-    int getAge();
+/* Abstract class: - A class containing at least one pure virtual
+ * function/abstract function is an abstract class
+ * - Instance/Object of abstract class cannot be made
+ * - Only objects of its subclass (if they are not abstract) can be made
+ * - Subclasses of an abstract base class must define the abstract method,
+ *   otherwise, they will also become abstract classes.
+ * - In an abstract class, we can also have other functions and variables apart
+ *   from pure virtual function.
+ * */
+class Person { // Abstract class
+    virtual int getAge(int x) = 0;
 };
 
 class Student: public Person {
@@ -12,7 +20,7 @@ public:
     Student(int s){
         age = s;
     }
-    int getAge(){
+    int getAge(int x){
         return age;
     }
 };
@@ -23,17 +31,19 @@ public:
     Faculty(int t){
         age = t;
     }
-    int getAge(){
+    int getAge(int a){
         return age;
     }
 };
 
 int main(){
+    // as long as we create an instance,
+    // we have to include abstract method inside derived class too
     Student s(123);
     Faculty f(23);
 
-    cout << "Age of Student: " << s.getAge() << endl;
-    cout << "Age of Faculty: " << f.getAge() << endl;
+    cout << "Age of Student: " << s.getAge(5) << endl;
+    cout << "Age of Faculty: " << f.getAge(6) << endl;
 
 
     return 0;
