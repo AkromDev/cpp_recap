a0ba1620c23ccf95bc6df4d3bfb6da40dc233135
Mukhammadali <toshboev7@gmail.com>
2018-06-13 23:45:23 +0900

    Abstract Class is done

--- Abstract_class.cpp
+++ Abstract_class.cpp
@@ -3,10 +3,37 @@
 using namespace std;
 
 class Person {
-    virtual
+    int getAge();
+};
+
+class Student: public Person {
+    int age;
+public:
+    Student(int s){
+        age = s;
+    }
+    int getAge(){
+        return age;
+    }
+};
+
+class Faculty: public Person {
+    int age;
+public:
+    Faculty(int t){
+        age = t;
+    }
+    int getAge(){
+        return age;
+    }
 };
 
 int main(){
+    Student s(123);
+    Faculty f(23);
+
+    cout << "Age of Student: " << s.getAge() << endl;
+    cout << "Age of Faculty: " << f.getAge() << endl;
 
 
     return 0;
