5829a5c68b1b50cccbad6dee4f405907896bda50
Mukhammadali <toshboev7@gmail.com>
2018-06-16 18:30:08 +0900

    Comments to Assignment3 have been added

--- assignment3.cpp
+++ assignment3.cpp
@@ -1,85 +1,99 @@
 #include <iostream>
-#include <array>
-
 using namespace std;
 
 class Matrix{
-    int **myArray;
-    int rowLength;
-    int colLength;
+    int **myArray; // pointer declaration to create 2D  array
+    int rowLength; // tracks the row length of  the array
+    int colLength; // tracks the column length of  the array
 public:
-    Matrix(){
+    Matrix(){ // default constructor
         int row , col;
         cout << "Enter row, col: ";
         cin >> row;
         cin >> col;
-        myArray = new int *[row];
+        myArray = new int *[row]; // create rows
         for (int i = 0; i < row; ++i) {
-            myArray[i] = new int [col];
-        }
-        rowLength = row;
-        colLength = col;
-    }
-    Matrix(int row, int col){
-        myArray = new int *[row];
-        for (int i = 0; i < row; ++i) {
-            myArray[i] = new int [col];
+            myArray[i] = new int [col]; //create columns
             for (int j = 0; j < col; ++j) {
-                myArray[i][j] = 0;
+                myArray[i][j] = 0; // by default  assign all values = 0;
             }
         }
+        // saves row and column length to private members
         rowLength = row;
         colLength = col;
     }
-    void getData(){
+    Matrix(int row, int col){ // Constructor with row and col parameters
+        myArray = new int *[row]; // create rows
+        for (int i = 0; i < row; ++i) {
+            myArray[i] = new int [col]; //create columns
+            for (int j = 0; j < col; ++j) {
+                myArray[i][j] = 0; // by default  assign all values = 0;
+            }
+        }
+        // saves row and column length to private members
+        rowLength = row;
+        colLength = col;
+    }
+    void getData(){ // function to get data from user
         for (int i = 0; i <rowLength ; ++i) {
             for (int j = 0; j < colLength; ++j) {
                 cin >> myArray[i][j];
             }
         }
     }
-    void show(){
+    void show(){ // function to display all values
         for (int i = 0; i <rowLength ; ++i) {
             for (int j = 0; j < colLength; ++j) {
                 cout  << myArray[i][j] << " ";
             }
-            cout << endl;
+            cout << endl; // to print the values in matrix format
         }
     }
+
+    /*
+     * @checkMatrices - receives 2 objects and checks whether obj1.rowLength == obj2.colLength
+     * and returns Boolean value;
+     * */
 
     bool checkMatrices(Matrix &obj1, Matrix &obj2){
         return obj1.colLength == obj2.rowLength;
     }
 
-    Matrix mul(Matrix &guestObj){
+    Matrix mul(Matrix &guestObj){ // function to multiply 2 Matrices
+        // @this - refers to current operating object (hostObj)
         try {
-            bool areMatricesValid = checkMatrices(*this, guestObj);
-            if(!areMatricesValid){
+
+            bool areMatricesValid = checkMatrices(*this, guestObj); // check Matrices
+            if(!areMatricesValid){ // if areMatricesValid is  false, throw error about Matrices row-column inequality
                 throw false;
             } else {
+                // Create an object with passed col and row length, to store the product of 2 arrays multiplication
                 Matrix product(this -> rowLength, guestObj.colLength);
 
-                for (int i = 0; i < this -> rowLength; ++i) {
-                    for (int j = 0; j < this -> colLength; ++j) {
-                        for (int k = 0; k < guestObj.colLength; ++k) {
+                for (int i = 0; i < this -> rowLength; ++i) { // loop for hostObj -> row
+                    for (int j = 0; j < this -> colLength; ++j) { // loop for hostObj -> column
+                        for (int k = 0; k < guestObj.colLength; ++k) { // loop for guestObj -> column
+
+                            // multiplies hostObj value with guestObj value, and stores into product 2-d array
                             product.myArray[i][k] += ((this->myArray[i][j]) * (guestObj.myArray[j][k]));
                         }
                     }
                 }
                 return product;
             }
-        } catch (bool areMatricesValid){
+
+        } catch (bool areMatricesValid){ // catch error about Matrices row-column inequality
             if(!areMatricesValid){
                 cout << "--------First Matrix col and Second Matrix row should be equal, please check!-------" << endl;
             }
         }
     }
 
-    ~Matrix(){
+    ~Matrix(){ // destrcutor
         for (int i = 0; i < rowLength ; ++i) {
-            delete myArray[i];
+            delete myArray[i]; // delete each column;
         }
-        delete [] myArray;
+        delete [] myArray; // delete whole array(columns)
     }
 };
 
